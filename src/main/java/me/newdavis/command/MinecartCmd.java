2ed4575eb77c1767b818c5007c0fe92e06b1fd94
Davis <davisharder@gmx.de>
2022-10-30 12:58:03 +0100

    Initial commit

---
+++ src/main/java/me/newdavis/command/MinecartCmd.java
@@ -0,0 +1,12 @@
+package me.newdavis.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class MinecartCmd implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
---
+++ src/main/java/me/newdavis/minecart/Minecart.java
@@ -0,0 +1,12 @@
+package me.newdavis.minecart;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecart extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
